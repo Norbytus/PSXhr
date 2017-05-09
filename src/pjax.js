f2391bb6b98401f15e9715cf76c6a5da97dc4028
norbytus <norbyt93@gmail.com>
2017-05-10 00:40:06 +0300

    Delete old class, set static method for auto create objects. Add propery with type answer. Add method for detect tag form. Add attr time and interval

--- src/pjax.js
+++ src/pjax.js
@@ -1,50 +1,57 @@
-class PjaxContainer {
-
-    constructor() {
-        this._pool = [];
-        this._getPjaxContainer();
-    }
-
-    _getPjaxContainer() {
-
-        document.querySelectorAll('[pjax-container="true"]').forEach( node => {
-            this._pool.push(new PjaxElement(node));
-        });
-
-    }
-
-}
-
 class PjaxElement {
+
+    static setPjax() {
+        document.querySelectorAll('[pjax="true"]').forEach( node => {
+            new PjaxElement(node));
+        });
+    }
 
     constructor(node) {
         this.attr = {};
+        this._response = {
+            array : 'arrayBuffer',
+            blob : 'blob',
+            form : 'formData',
+            json : 'json',
+            text : 'text'
+        };
         this.node = node;
         this._data = new FormData();
-        this._setData();
         this._checkAttribute(node);
         this._setEvent();
     }
 
     _checkAttribute() {
 
-        let attributeList = [ 'container', 'event', 'href', 'method', 'state' ];
         let result = {};
-        attributeList.map( attr => {
+
+        if (this._isForm()) this._setData();
+
+        let attrList = [
+            'container',
+            'event',
+            'href',
+            'method',
+            'state',
+            'callback',
+            'interval',
+            'time',
+            'response',
+        ];
+
+        attrList.map( attr => {
             result[attr] = this.node.hasAttribute('pjax-' + attr)
                 ? this.node.getAttribute('pjax-' + attr)
                 : false;
         });
-        this.attr = result;
+
+        this.attr = Object.assign(this.attr, result);
 
     }
 
-    _setData() {
+    _isForm() { return this.node.tagName == 'FORM'; }
 
-        if (this.node.tagName == 'FORM')
-            this._data = new FormData(this.node);
-
-    }
+    _setData() { this._data = new FormData(this.node); }
 
     _setEvent() {
 
@@ -57,13 +64,19 @@ class PjaxElement {
             node.addEventListener(event, this._eventWrapper.bind(this), false);
         });
 
+        if (this.attr.interval)
+            setInterval(this._eventWrapper.bind(this), this.attr.time);
+
+        if (this.attr.time)
+            setTimeout(this._eventWrapper.bind(this), this.attr.time);
+
     }
 
     _eventWrapper(e) {
 
         this._Ajax(this.attr.href, this.attr.method);
 
-        e.preventDefault();
+        if (e) e.preventDefault();
 
     }
 
@@ -73,22 +86,44 @@ class PjaxElement {
             method: method ? method : 'GET',
             headers: {'X-REQUESTED-WITH': 'XMLHttpRequest'},
         };
+
+        if (data.method == 'POST') data.body = this._data;
+
         var href = href ? href : window.location.href;
 
         fetch(href, data)
             .then( res => {
-                return res.text();
+
+                let responseType = this._response[this.attr.response]
+                    ? this._response[this.attr.response]
+                    : 'text';
+
+                return res[this.attr.response]();
+
             })
             .then( res => {
-                this.node.innerHTML = res;
-                if (JSON.parse(this.attr.state)) {
+
+                this._defaultHandler(res);
+
+                if (JSON.parse(this.attr.state))
                     history.pushState({test: 'test'}, '', href);
-                }
 
             })
             .catch( err => {
                 console.log(err);
             })
+
+    }
+
+    _defaultHandler(res) {
+
+        if (!this.attr.container)
+            this.node.innerHTML = res;
+        else {
+            let container = document.querySelector(this.attr.container);
+            if (!container) return false;
+            container.innerHTML = res;
+        }
 
     }
 
