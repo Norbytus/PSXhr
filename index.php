7724b7cc508e04df200f985b78327e03532bea3a
norbytus <norbyt93@gmail.com>
2017-05-27 02:08:26 +0300

    Delete old PSXhr.js file small change in index.php for test

--- index.php
+++ index.php
@@ -13,21 +13,27 @@ if (isset($_GET['get-date'])) {
     <title></title>
 </head>
 <body>
-    <div class="cont" psxhr="true" psxhr-href="<?= $_SERVER['PHP_SELF']; ?>?get-date=true" psxhr-time="1000" psxhr-promise="testPromis" psxhr-event="click">
+    <div class="cont" psxhr="true" psxhr-href="<?= $_SERVER['PHP_SELF']; ?>?get-date=true" psxhr-time="1000" psxhr-callback="testCallback" psxhr-event="click">
         This block must be restart
     </div>
-    <a psxhr="true" psxhr-container=".cont" href="<?= $_SERVER['PHP_SELF']; ?>?get-date=true" psxhr-event="click">Link</a>
+    <!--<a psxhr="true" psxhr-container=".cont" href="<?= $_SERVER['PHP_SELF']; ?>?get-date=true" psxhr-time="1000" psxhr-event="click">Link</a>
     <form class="cont" psxhr="true" method="GET" action="<?= $_SERVER['PHP_SELF']; ?>" psxhr-state="true" psxhr-event="submit">
         <input id="" type="text" name="test" value="Hello">
         <input id="" type="text" name="tes[]" value="ello">
         <input id="" type="text" name="tes[]" value="Hello">
         This block must be restart
         <input type="submit" value="send" name="test">
-    </form>
+    </form>-->
 </body>
 <script src="public/PSXhr.min.js"></script>
 <script>
 PSXhr.init();
+
+function testCallback(response) {
+
+
+
+}
 
 function testPromis(promise) {
 
